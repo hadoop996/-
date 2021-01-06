a8c16dc58647c39fc3bf9175c95dfaf61b188193
郝少杰 <haoshaojie@tydic.com>
2021-01-07 03:34:11 +0800

    财富自由第一步

---
+++ src/main/java/com/worker/demo/SayHello.java
@@ -0,0 +1,11 @@
+package com.worker.demo;
+
+/**
+ * @author 郝少杰
+ * @date 2021/1/7 3:32
+ */
+public class SayHello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
