8e1e3fe8bafbed68e255b545d0836bb5646931fd
郝少杰 <haoshaojie@tydic.com>
2021-01-07 03:41:02 +0800

    测试

--- src/main/java/com/worker/demo/SayHello.java
+++ src/main/java/com/worker/demo/SayHello.java
@@ -7,5 +7,6 @@ package com.worker.demo;
 public class SayHello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("测试");
     }
 }
